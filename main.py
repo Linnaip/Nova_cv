56450699bea391d89a3f60782061175677a2710c
Andrey Mac <linnaip@yandex.ru>
2023-02-23 14:25:50 +0300

    Рефакторинг

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import executor
 
-from bot_app.app import dispatcher
+from src.bot_app.app import dispatcher
+
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher, skip_updates=True)
--- telegram_bot/telegram_bot/asgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telegram_bot.settings')
-
-application = get_asgi_application()
